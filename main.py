b4b017e14fa8033e1dbb725d76e1c86434fa14d6
Shawn T. Brown <stbrown@psc.edu>
2021-09-02 08:15:20 -0400

    Added doc strings

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
+"""
+simple-python-example
+
+A program that is meant to be used as an example in class
+"""
 import os
+from message import Message
 
 def print_message_from_file(filename):
+    """
+    Prints a message from contents for a <fieldset>
+
+    Arguments:
+            filename - A string giving the name of the files that is in the current
+                       directory
+
+    Returns:
+      Returns no value, prints the message to stdout
+    """
     with open(filename, "r") as f_to_read:
         message = f_to_read.read()
         print("This is the message")
         print("{}".format(message))
 
 def print_separator(num_chars=45):
+    """
+    Prints a separator of lines
+
+    Arguments:
+        num_chars: number of hyphens to put in the print_separator
+
+    Returns:
+        No Value, prints the separator to stdout
+    """
     print("{}".format("".join(["-" for x in range(0, num_chars)])))
 
 def main():
+    """
+    The main function that runs the program
+    """
     for i in range(0, 10):
         print("Hello World!#{}".format(i))
 
@@ -44,6 +72,29 @@ def main():
     print_message_from_file(filename)
 
     print_separator()
+
+    msg = Message("... this is in class.")
+    msg.print_message()
+
+    print_separator()
+
+    print("{}".format(msg))
+
+    print_separator()
+
+    msg_add = msg + " | adding a string"
+    msg_add.print_message()
+
+    print_separator()
+
+    msg2 = Message("... adding a message class")
+    msg_add = msg + msg2
+    msg_add.print_message()
+
+    print_separator()
+
+    num = 6.003432
+    msg_add = msg + num
 
 if __name__ == "__main__":
     main()
--- message.py
+++ message.py
@@ -1,6 +1,53 @@
+"""
+message.property
+
+Contains the message class to handle messages from the files
+"""
 class Message:
+    """
+    Contains a message the can be printed out to the screen
+    """
     def __init__(self, message_string):
+        """
+        Creates an instance of messsage
+
+        Arguments:
+
+        message_string: string containing the message
+                        to be displayed
+
+        Returns:
+
+        instance of message
+        """
         self.message_string = message_string
 
     def print_message(self):
+        """
+        Prints the message to stdout
+        """
         print("This is the class message {}".format(self.message_string))
+
+    def __str__(self):
+        """
+        Overrides the str operator to print a pretty string
+        """
+        return "This is overriding the str class of the fuction: {}".format(self.message_string)
+
+    def __add__(self, msg):
+        """
+        Overrides the add operator to either add a Message or string
+
+        Arguments:
+            msg - A Message instance or string that is to be added
+
+        Returns:
+            New Message instance with msg added to the message_string
+        """
+        if isinstance(msg, str):
+            return Message(self.message_string + msg)
+        elif isinstance(msg, Message):
+            return Message(self.message_string + msg.message_string)
+        else:
+            raise TypeError("Class Message does not know how to add" +
+                            "a variable of type {}".format(type(msg)))
